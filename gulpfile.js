3e3f59659e946d7277d29acb64f2804b3cd42a0c
bellangerq <bellanger.wall@gmail.com>
2017-07-24 15:52:50 +0200

    Setup Gulp 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,2 +1,12 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+
+gulp.task('sass', function() {
+  gulp.src('stylesheets/main.scss')
+    .pipe(sass().on('error', sass.logError))
+    .pipe(gulp.dest('stylesheets/'));
+});
+
+gulp.task('default',function() {
+  gulp.watch('stylesheets/*.scss',['sass']);
+});
